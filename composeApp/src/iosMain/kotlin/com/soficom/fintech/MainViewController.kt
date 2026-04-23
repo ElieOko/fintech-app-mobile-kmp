e30207bd46b286ccdc84b47559ce7a6de8203321
ElieOko <elieoko100@gmail.com>
2026-04-23 07:59:28 -0700

    fix:comment

--- composeApp/src/iosMain/kotlin/com/soficom/fintech/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/soficom/fintech/MainViewController.kt
@@ -2,4 +2,4 @@ package com.soficom.fintech
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-//fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { App() }
