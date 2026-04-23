dccba4f5b8acb732582a0f9fa17e82905ce4a0a8
ElieOko <elieoko100@gmail.com>
2026-04-23 07:58:28 -0700

    fix:call page onboarding

--- composeApp/src/commonMain/kotlin/com/soficom/fintech/App.kt
+++ composeApp/src/commonMain/kotlin/com/soficom/fintech/App.kt
@@ -1,23 +1,16 @@
 package com.soficom.fintech
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.painterResource
-
-import fintech.composeapp.generated.resources.Res
-import fintech.composeapp.generated.resources.compose_multiplatform
+import com.soficom.fintech.pages.OnboardingPage
 
 @Composable
 @Preview
@@ -26,24 +19,12 @@ fun App() {
         var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
+                .background(Color(0xFFF1E9E9))
                 .safeContentPadding()
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+            OnboardingPage()
         }
     }
 }
